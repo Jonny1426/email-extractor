bac600481a60f03b3779a349e72e2af15dfcfb8a
Jonny Melo <50029211+Jonny1426@users.noreply.github.com>
2020-01-21 15:51:52 -0300

    Adição do script

---
+++ extrator.py
@@ -0,0 +1,30 @@
+import re
+import requests 
+
+try:
+    r = requests.get('')
+   
+    if r.status_code == requests.codes.ok:
+        print('Site acessado com sucesso')
+        print('-'*15)
+
+        padrao = re.findall(r'[\w\.-]+@[\w-]+\.[\w\.-]+', r.text)
+        
+        if padrao:
+            print('Quantidade de emails encontrados:', len(padrao))
+            print(padrao)
+
+            arquivo = open('email.txt', 'w')
+
+            for email in padrao:
+                arquivo.write(email+'\n')
+            arquivo.close()
+
+        else:
+            print('padrao não encontrado')
+
+    else:
+        print('Erro ao tentar acessar o site. Código de resposta:', r.status_code)
+
+except Exception as e:
+    print('Erro: ', e)
